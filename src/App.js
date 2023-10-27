6f7c8e5cada8195ca9cc45ac894c44c74aeeda10
Griffin Amaral <griffin.amaral98@gmail.com>
2023-10-26 20:38:36 -0400

    Created a date counter using react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
     </div>
   );
 }
 
 export default App;
+
+const Counter = () => {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+  let days = count;
+
+  const addDays = () => {
+    const date = new Date();
+    const addCountToDate = date.getDate() + count;
+    date.setDate(addCountToDate);
+    const formatted = date.toLocaleDateString();
+    return formatted;
+  };
+
+  return (
+    <div>
+      <div>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+        {step}
+      </div>
+
+      <div>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+      </div>
+
+      <span>
+        {count === 0
+          ? `Today is ${addDays()}`
+          : count > 0
+          ? `${days} days from today is ${addDays()}`
+          : `${Math.abs(count)} days ago was ${addDays()}`}
+      </span>
+    </div>
+  );
+};
